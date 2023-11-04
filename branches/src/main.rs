7fdeea19b2dd507e964d9b99e7f683c8947d19a5
Bo Kamphues <b.kamphues@outlook.com>
2023-11-04 18:07:33 +0100

    Added loops example

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let number: u8 = 12;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
